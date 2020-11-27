f9065cad0a3886477b7e82b52ec2bb1eb6e31c20
Victor Martinez <vcrmartinez@gmail.com>
2020-11-27 19:52:36 -0300

    feat: reset CSS with global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,8 @@ import Hero from "./Hero"
 import TopBar from "./TopBar"
 import Footer from "./Footer"
 
+import GlobalStyles from "../styles/global"
+
 import * as S from "./MainWrapper/styled"
 
 const Layout = ({ location, title, children }) => {
@@ -13,38 +15,13 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <>
+      <GlobalStyles />
       <TopBar />
       <Hero showBackgroundImage={isRootPath} />
       <S.MainWrapper>{children}</S.MainWrapper>
       <Footer />
     </>
   )
-
-  // let header
-
-  // if (isRootPath) {
-  //   header = (
-  //     <h1 className="main-heading">
-  //       <Link to="/">{title}</Link>
-  //     </h1>
-  //   )
-  // } else {
-  //   header = (
-  //     <Link className="header-link-home" to="/">
-  //       {title}
-  //     </Link>
-  //   )
-  // }
-  // <div className="global-wrapper" data-is-root-path={isRootPath}>
-
-  //   <header className="global-header">{header}</header>
-  //   <main>{children}</main>
-  //   <footer>
-  //     © {new Date().getFullYear()}, Built with
-  //     {` `}
-  //     <a href="https://www.gatsbyjs.com">Gatsby</a>
-  //   </footer>
-  // </div>
 }
 
 export default Layout
