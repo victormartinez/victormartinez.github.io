7589e5907505da121ac815864b5467ee5ab5386f
Victor Martinez <vcrmartinez@gmail.com>
2020-12-11 16:59:05 -0300

    feat: adjust paragraph for tablet

--- src/components/Paragraph/styled.js
+++ src/components/Paragraph/styled.js
@@ -4,8 +4,9 @@ import media from "styled-media-query"
 export const Paragraph = styled.p`
   width: 70em;
 
-  ${media.lessThan("small")`
+  ${media.lessThan("medium")`
     width: 100vw;
     padding: 0 1.5em;
+    margin-top: 2em;
   `}
 `
