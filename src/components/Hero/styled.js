6d106f78c67f15a0a2b0a3e5869fc7f43957b994
Victor Martinez <vcrmartinez@gmail.com>
2020-12-17 23:14:24 -0300

    fix: replace background url by source image

--- src/components/Hero/styled.js
+++ src/components/Hero/styled.js
@@ -1,11 +1,13 @@
 import media from "styled-media-query"
 import styled from "styled-components"
 
+import officeBackground from "../../../content/assets/office-background.jpeg"
+
 export const HeroWrapperBg = styled.div`
   display: flex;
   background-color: #000;
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url("https://images.unsplash.com/photo-1510519138101-570d1dca3d66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1931&q=80");
+    url(${officeBackground});
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
