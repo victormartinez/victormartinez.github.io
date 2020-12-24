f064b6b389c12a78124dde4639a47553216437ac
Victor Martinez <vcrmartinez@gmail.com>
2020-12-24 19:39:22 -0300

    feat: sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,6 +80,7 @@ module.exports = {
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
