d8bf4f6ee63dea6b68dc6a976cbfaa53dfaacd56
Victor Martinez <vcrmartinez@gmail.com>
2022-10-26 18:18:05 -0300

    style: adjust Alert css

--- src/components/Alert/styled.js
+++ src/components/Alert/styled.js
@@ -4,9 +4,9 @@ export const AlertWrapper = styled.div`
   width: 100%;
   background-color: #fff3cd;
   color: #856404;
-  padding: 0.5rem 1.25rem;
+  padding: 0.25rem 1.25rem;
   border-radius: 0.25rem;
   margin-top: 1.5em;
-  font-size: 0.8rem;
+  font-size: 1rem;
   text-align: center;
 `
