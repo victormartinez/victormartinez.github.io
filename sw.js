134b54e19f79d65691a3c74e001235edbe2b5b24
Victor Martinez <vcrmartinez@gmail.com>
2022-10-26 09:10:25 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d688bdd9ae1a5f04df41.js"
+    "url": "webpack-runtime-f9a6db2a34856a7e0e2d.js"
   },
   {
     "url": "styles.e2dd2cebf4a6423f8bfb.css"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "framework-58f863d6aa18c11a521f.js"
   },
   {
-    "url": "app-534de46bd6c91aab3d04.js"
+    "url": "app-3e4a67f0cd201430ee5b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e48a729e6df03cc78e1b6cb285d75cbe"
+    "revision": "65ab6cf5d2b7b1d5fe21f02cdc8ac8e6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "552c06171e721e3034432462e873f788"
+    "revision": "5ffabb1cea1a67cc37e6c827bff2720d"
   },
   {
     "url": "polyfill-3f99b51aa82fcaeb5543.js"
@@ -153,7 +153,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-534de46bd6c91aab3d04.js`))) {
+  if (!resources || !(await caches.match(`/app-3e4a67f0cd201430ee5b.js`))) {
     return await fetch(event.request)
   }
 
